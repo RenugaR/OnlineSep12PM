62acfc5722acc0014810129a9ede81073199c803
RenugaR <49983046+RenugaR@users.noreply.github.com>
2021-09-11 11:51:25 +0530

    Update LibGlobal.java

--- src/test/java/com/utils/LibGlobal.java
+++ src/test/java/com/utils/LibGlobal.java
@@ -16,7 +16,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-r;
 
 public class LibGlobal {
 	public static WebDriver driver;
